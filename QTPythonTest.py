67815dc3dcd343722d64cbf1f6d0bf48b73053e7
RibelH <robert.hemenguel@hotmail.com>
2020-02-04 01:11:23 +0100

    + Buttons/stylesheet

--- QTPythonTest.py
+++ QTPythonTest.py
@@ -12,17 +12,43 @@ def MainWindow():
     w.setWindowTitle("My first QtTest")
     ic = "C:/Users/Robert/Pictures/weather.png"
     w.setWindowIcon(QIcon(ic))
-    #Label
+    stylesheet = """
+        QPushButton {
+            font: bold; 
+            background-color: black; 
+            font-size:12px; 
+            height:40px;
+            width:100px; 
+            color:white;
+            border: 2px solid #646262
+        }
+        QPushButton:hover {
+            background-color: #C0C0C0;
+            color: black;
+        }
+        QPushButton:pressed {
+            background-color: #6C6C6C;
+            color: white
+        }
+    """
+    app.setStyleSheet(stylesheet)
+    #Label creating
     label = QLabel(w)
     label.setText("Hello")
     label.move(50,20)
+
     #Button
-    b1 = QPushButton(w,text= "Button 1" )
-    # b1.setText("Button 1")
-    b1.move(100,100)
+    b1 = QPushButton(w, text="Button 1")
+    b1.move(100, 100)
+    b1.setObjectName("b1_button")
+
+    b2 = QPushButton(w, text="Button 2")
+    b2.move(300,100)
+
+
 
     #Button signal linking
-    b1.clicked.connect(lambda: b1_clicked("B1"))
+    b1.clicked.connect(lambda: b1_clicked("You just clicked Button 1"))
 
     w.show()
     sys.exit(app.exec_())
