f865bcbc35a6f04b59d28b287c1c26099640bf8d
RibelH <robert.hemenguel@hotmail.com>
2020-07-30 16:47:46 +0200

    + added Test-Documentation + added additional Comments + improved readability of Code + added "capitalizing()" function

--- QtPyWeather.py
+++ QtPyWeather.py
@@ -10,12 +10,18 @@ import sys
 
 
 class MainWindow(QMainWindow):
-    city = "1"
+    """Initializing MainWindow and all functions needed"""
+#-----------------------__INIT__.START----------------------#
+    city = ""
+
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
         global city
+        #Windowtitle
         self.setWindowTitle("QtPyWeatherAPP")
+        #WindowIcon
         self.setWindowIcon(QtGui.QIcon("C:/Users/Robert/Pictures/weather.png"))
+        #Stylesheet stored in variable
         self.stylesheet = """
         
         QLineEdit{
@@ -44,7 +50,7 @@ class MainWindow(QMainWindow):
         #Default City
         city =""
 
-
+        #Stores weather data in list
         list = self.info(city)
 
         #Resizing window
@@ -55,8 +61,8 @@ class MainWindow(QMainWindow):
         self.button_change_city.clicked.connect(lambda:self.label.setText(self.update_temp(self.textbox.text())))
         self.button_change_city.clicked.connect(lambda: self.desc_label.setText(self.update_desc(self.textbox.text())))
         self.button_change_city.clicked.connect(lambda: self.label_t.setText(self.update_title(self.textbox.text())))
-        self.button_change_city.clicked.connect(lambda: self.textbox.setText(""))
         self.button_change_city.clicked.connect(lambda: self.update_city(self.textbox.text()))
+        #self.button_change_city.clicked.connect(lambda: self.textbox.setText(""))
 
         #Textbox creation
         self.textbox = QLineEdit(self)
@@ -67,7 +73,6 @@ class MainWindow(QMainWindow):
         #self.label Title creation
         self.label_t = QLabel(list[2])
         self.label_t.setAlignment(Qt.AlignHCenter | Qt.AlignVCenter)
-        
         self.label_t.setStyleSheet("font-size:20px; background-color:#a5f8ff")
 
         #self.label creation
@@ -87,13 +92,13 @@ class MainWindow(QMainWindow):
         image = QPixmap(self.img_d(list[1]))
         self.label_img.setPixmap(image)
         self.label_img.setAlignment(Qt.AlignHCenter | Qt.AlignVCenter)
-
         self.label_img.setStyleSheet("background-color:#78ECF6")
 
 
 
         #Layout Creation
         layout = QVBoxLayout()
+
         #Add Widget to Layout
         layout.addWidget(self.textbox)
         layout.addWidget(self.button_change_city)
@@ -121,8 +126,10 @@ class MainWindow(QMainWindow):
         button_action.triggered.connect(lambda: self.onMyToolBarButtonClick("Hallo"))
         button_action.setCheckable(True)
         toolbar.addAction(button_action)
+
         #Separtor creation
         toolbar.addSeparator()
+
         #Button2 creation
         # TODO: Refresh Button for new Temperature
         Refresh_button = QAction(QIcon("C:/Gui/arrow-circle-double"), "Refresh Stats", self)
@@ -131,26 +138,30 @@ class MainWindow(QMainWindow):
         Refresh_button.triggered.connect(lambda: self.desc_label.setText(self.update_desc(city)))
         Refresh_button.setCheckable(False)
         toolbar.addAction(Refresh_button)
+
         #Speparator
         toolbar.addSeparator()
+
         #Adding Widget to Toolbar
         toolbar.addWidget(QLabel("Hello"))
         toolbar.addWidget(QCheckBox())
-
         self.setStatusBar(QStatusBar(self))
 
 
         #Menu creation
         menu = self.menuBar()
-
         file_menu = menu.addMenu("&File")
         file_menu.addAction(button_action)
         file_menu.addSeparator()
 
+        #Submenu creation
         file_submenu = file_menu.addMenu("Submenu")
         file_submenu.addAction(Refresh_button)
 
+        #Setting Stylesheet
         app.setStyleSheet(self.stylesheet)
+    #-------------------------__INIT__.END-------------------------#
+
     #Function to update Title
     def update_title(self, cit):
         l = self.info(cit)
@@ -192,8 +203,9 @@ class MainWindow(QMainWindow):
 
     #Function to get Weather Data
     def info(self, cit = "Kreuzlingen"):
-        #Get weather data
+        #Get weather data with Exceptionhandling
         try:
+            #Try to get DATA with cit variable
             city = cit
             url = "http://api.openweathermap.org/data/2.5/weather?q={}&appid=ac7c75b9937a495021393024d0a90c44&units=metric".format(city)
             res = requests.get(url)
@@ -201,11 +213,12 @@ class MainWindow(QMainWindow):
 
             #Store weather Data
             temp = str(data["main"]["temp"])
-            desc = data["weather"][0]["description"]
-            result = "{}".format(city)
+            desc = self.capitalizing(data["weather"][0]["description"])
+            result = self.capitalizing("{}".format(city))
 
             return temp, desc, result
         except:
+            #If cit variable gives KeyError get data of Default City
             city = "Kreuzlingen"
             url = "http://api.openweathermap.org/data/2.5/weather?q={}&appid=ac7c75b9937a495021393024d0a90c44&units=metric".format(
                 city)
@@ -214,20 +227,40 @@ class MainWindow(QMainWindow):
 
             # Store weather Data
             temp = str(data["main"]["temp"])
-            desc = data["weather"][0]["description"]
-            result = "{}".format(city)
-
+            desc = self.capitalizing(data["weather"][0]["description"])
+            result = self.capitalizing("{}".format(city))
             return temp, desc, result
+
+    #Function updates current city value
     def update_city(self, cit):
         global city
-        print(city)
         city = cit
 
+    #Capitalizing Output Strings
+    def capitalizing(self,long_string):
+        """Capitalizing all words of the given string"""
+        words = long_string.split()
+        if len(words)>1:
+            cap_words = [word.capitalize() for word in words if len(words) > 1]
+            s = " ".join(cap_words)
+            return s
+        else:
+            s = long_string.capitalize()
+            return s
 
 
+#--------------------------------------------------------------------------------------#
+#initializing window
 app = QApplication(sys.argv)
-
 window = MainWindow()
+
+#Displaying Window
 window.show()
 
+#Documentation -----TEST------
+print(window.__doc__)
+print(window.capitalizing.__doc__)
+
 app.exec_()
+
+#----------------------------------------------------------------------------------------#
